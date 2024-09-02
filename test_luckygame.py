2408758d6238c85c4c6edcede7df94d7bb38b850
GabeLack <gabriel.lackgren@gmail.com>
2024-09-03 00:49:04 +0200

    setUp for test class

--- test_luckygame.py
+++ test_luckygame.py
@@ -3,3 +3,15 @@ import unittest
 from luckygame import LuckyGame
 from details import Details
 
+class TestLuckyGame(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a fresh instance of LuckyGame for each test."""
+        # Instancing
+        self.d = Details()
+        self.d.in_name('John Doe')
+        self.d.in_birth('19510101')
+        self.lg = LuckyGame()
+
+if __name__ == '__main__':
+    unittest.main()
